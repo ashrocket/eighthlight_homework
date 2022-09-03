064f4620af217d208fe22f53ff6b50896869b3ad
Ashley Raiteri <ashley@raiteri.net>
2022-09-02 20:20:38 -0400

    quick synrtax fix

--- spec/services/list_unique_service_spec.rb
+++ spec/services/list_unique_service_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe ListUniqueService, type: :model do
   describe '#call' do
     it 'removes all dups and preserves order' do
       result = ListUniqueService.new("11223344ffgghhjjmississippi").call
-      expect(result).to eql? "1234fghjmisp"
+      expect(result).to eql "1234fghjmisp"
     end
     it 'returns an empty list when given and empty list' do
       result = ListUniqueService.new("").call
-      expect(result).to eql? ""
+      expect(result).to eql ""
     end
     it 'returns original list when given a list with no duplicastes' do
       no_dups = "12345690aghjioebm<%"
       result = ListUniqueService.new(no_dups).call
-      expect(result).to eql? no_dups
+      expect(result).to eql no_dups
     end
   end
 end
